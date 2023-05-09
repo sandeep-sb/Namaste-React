24c00785ee15545052865c138450b50fc5de73c0
Sandeep S Bhadouria <iamssb99@gmail.com>
2023-05-09 23:43:49 +0530

    creating complex structure with React.createElement

--- Lecture-1/App.js
+++ Lecture-1/App.js
@@ -1,9 +1,35 @@
-const heading = React.createElement(
-    "h1",
-    {
-        id:"heading",
-     }, 
-    "Hello World from Namaste React");
+/**
+ *
+ * <div id="parent">
+ *      <div id="child1">
+ *          <h1>This is an h1 tag</h1>
+ *          <h2>This is an h2 tag</h2>
+ *      </div>
+ *      <div id="child2">
+ *          <h1>This is an h1 tag</h1>
+ *          <h2>This is an h2 tag</h2>
+ *      </div>
+ * </div>
+ *
+ */
+
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "This is an h1 tag"),
+    React.createElement("h2", {}, "This is an h2 tag")
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "This is an h1 tag"),
+    React.createElement("h2", {}, "This is an h2 tag")
+  ])
+]);
+
+// const heading = React.createElement(
+//     "h1",
+//     {
+//         id:"heading",
+//      },
+//     "Hello World from Namaste React");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

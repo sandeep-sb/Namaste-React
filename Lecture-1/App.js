833a5a29c813ba658582b0306450a8df4c9e79c0
Sandeep S Bhadouria <iamssb99@gmail.com>
2023-05-18 00:28:50 +0530

    react component

--- Lecture-1/App.js
+++ Lecture-1/App.js
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // react element
-const heading = <h1>Namaste React with JSX🚀</h1>;
+const heading = <h1 id="heading">Namaste React with JSX🚀</h1>;
+
+// react component 
+const HeadingComponent = () => {
+    return <h1>Namaste react Heading Component</h1>;
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<HeadingComponent/>);

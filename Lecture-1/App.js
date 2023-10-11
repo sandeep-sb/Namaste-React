f34278e63a22e9b53955ce9705a6050e0fc6c10c
Sandeep S Bhadouria <iamssb99@gmail.com>
2023-10-11 18:37:36 +0530

    components and elements

--- Lecture-1/App.js
+++ Lecture-1/App.js
@@ -3,13 +3,22 @@ import ReactDOM from "react-dom/client";
 
 const TitleComponent = () => <h1 id="heading">Namaste React with JSX🚀</h1>;
 
-const HeadingComponent = () => {
-    return <div>
-        <TitleComponent />
-        <h1>Namaste react Heading Component</h1>
-    </div>;
-}
+// heading component using return and curly braces.
 
+// const HeadingComponent = () => {
+//     return <div>
+//         <TitleComponent />
+//         <h1>Namaste react Heading Component</h1>
+//     </div>;
+// }
+
+// heading component not using return and using parenthesis 
+const HeadingComponent = () => (
+    <div>
+        <TitleComponent/>
+        <h1>Namaste React Heading Component </h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<HeadingComponent/>);

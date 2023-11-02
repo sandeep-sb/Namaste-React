d5acbce96b031582ddc1d32100a3976abdd1a243
Sandeep S Bhadouria <iamssb99@gmail.com>
2023-11-02 21:30:15 +0530

    login btn added

--- Lecture-1/src/components/Header.js
+++ Lecture-1/src/components/Header.js
@@ -1,14 +1,13 @@
 import { LOGO_URL } from "../utils/constants";
+import { useState } from "react";
 
 const Header = () => {
+  const [loginBtn, setLoginBtn] = useState("Login");
+
   return (
     <div className="header">
       <div className="logo-container">
-        <img
-          className="logo"
-          src= {LOGO_URL}
-          alt="burger logo with crown"
-        />
+        <img className="logo" src={LOGO_URL} alt="burger logo with crown" />
       </div>
       <div className="nav-items">
         <ul>
@@ -16,6 +15,16 @@ const Header = () => {
           <li>About Us</li>
           <li>Contact Us</li>
           <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              loginBtn === "Login"
+                ? setLoginBtn("Logout")
+                : setLoginBtn("Login");
+            }}
+          >
+            {loginBtn}
+          </button>
         </ul>
       </div>
     </div>

c0128dd100454360eae1ffcf6cdb04c92fc49474
Sandeep S Bhadouria <iamssb99@gmail.com>
2023-11-02 23:06:29 +0530

    search functionality added

--- Lecture-1/src/components/Body.js
+++ Lecture-1/src/components/Body.js
@@ -4,6 +4,8 @@ import Shimmer from "./Shimmer";
 
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     console.log("Use Effect called");
@@ -11,19 +13,51 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await data.json();
-    setListOfRestaurants(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-};
+    setListOfRestaurants(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
 
-// // Conditional Rendering
-//   if(listOfRestaurants.length === 0){
-//     return (<Shimmer />);
-//  }
+    setFilteredRestaurant(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
-  return listOfRestaurants.length === 0 ? <Shimmer /> : (
+  // // Conditional Rendering
+  //   if(listOfRestaurants.length === 0){
+  //     return (<Shimmer />);
+  //  }
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
-      <div className="search-bar">Search</div>
+      <div className="search-bar">
+        <input
+          className="search-box"
+          type="text"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (restaurant) => {
+                return restaurant.info.name.toLowerCase().includes(searchText);
+              }
+            );
+            setFilteredRestaurant(filteredList);
+          }}
+        >
+          Search
+        </button>
+      </div>
       <div>
         <button
           className="filter-btn"
@@ -39,7 +73,7 @@ const Body = () => {
         </button>
       </div>
       <div className="res-card-container">
-        {listOfRestaurants.map((restaurant) => (
+        {filteredRestaurant.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
